b6fd4ac31815946168f32c9eb3c97e1f3ba2976f
shundroid <ra24483@qb3.so-net.ne.jp>
2015-12-29 20:57:20 +0900

    fixed #15

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var less = require("gulp-less");
 var jade = require("gulp-jade");
 var buffer = require("vinyl-buffer");
 var minimist = require("minimist");
+var watchify = require("watchify");
 
 function find(pattern) {
   return new Promise(resolve => {
@@ -33,24 +34,56 @@ gulp.task("build", function() {
   gulp.watch("./css/*.less", ["less"]);
   gulp.watch("./*.jade", ["jade"]);
 });
-gulp.task("mountain", function() {
-  var env = minimist(process.argv.slice(2));
-  find("./js/{main.ts,ui.ts,modules/**/*.ts}").then(files => {
-    console.log(files);
-    var f = browserify({
-      entries: files
-    }).plugin(tsify, {
-      noImplicitAny: true,
-      target: "es5"
-    }).bundle();
-    if (env.dev) {
-      f.pipe(source("./all.js")).pipe(gulp.dest("./js/"))
-    } else {
-      f.pipe(source("./all.min.js")).pipe(buffer()).pipe(uglify()).pipe(gulp.dest("./js/"))
-    }
-    console.log("fuga");
+gulp.task("browserify", function(callBack) {
+  rify(false).then(() => {
+    callBack();
   });
 });
+gulp.task("w-browserify", function(callBack) {
+  rify(true).then(() => {
+    callBack();
+  });
+});
+function rify(isWatch) {
+  return new Promise((resolve) => {
+    var env = minimist(process.argv.slice(2));
+    var isDev = env.dev;
+    find("./js/{main.ts,ui.ts,modules/**/*.ts}").then(files => {
+      console.log(files);
+      var options = {
+        entries: files
+      };
+      var bundler = null;
+      if (isWatch) {
+        options.cache = {};
+        options.packageCache = {};
+        bundler = watchify(browserify(options));
+      } else {
+        bundler = browserify(options);
+      }
+
+      function bundle() {
+        var f = bundler.plugin(tsify, {
+          noImplicitAny: true,
+          target: "es5"
+        }).bundle();
+        if (isDev) {
+          return f.pipe(source("./all.js")).pipe(gulp.dest("./js/"))
+        } else {
+          return f.pipe(source("./all.min.js")).pipe(buffer()).pipe(uglify()).pipe(gulp.dest("./js/"))
+        }
+      }
+      if (isWatch) {
+        bundler.on("update", bundle);
+        bundler.on("log", function(msg) {
+          console.log(msg);
+        });
+      }
+      bundle();
+      resolve();
+    });
+  });
+}
 gulp.task("deljs", function() {
   del(["./js/{main.js,modules/*.js}"]);
 });

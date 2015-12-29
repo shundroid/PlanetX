b1ce5ff9aa2d353a6e7f9ce36c4190f8ba6952fd
shundroid <ra24483@qb3.so-net.ne.jp>
2015-12-29 11:25:03 +0900

    delete js_temp

--- js_temp/modules/compiler.js
+++
@@ -1,8 +0,0 @@
-var compiler;
-(function (compiler) {
-    function convertOldFile(oldFile) {
-        return "";
-    }
-    compiler.convertOldFile = convertOldFile;
-})(compiler || (compiler = {}));
-module.exports = compiler;
--- js_temp/modules/importJS.js
+++
@@ -1,6 +0,0 @@
-function importJS(src) {
-    var elem = document.createElement("script");
-    elem.src = src;
-    return elem;
-}
-module.exports = importJS;
